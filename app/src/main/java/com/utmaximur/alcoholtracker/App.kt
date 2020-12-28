2cb9a1aee61b79e8b403656282bf1aa22d68f386
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2020-12-28 22:23:19 +0700

    Добавлен выбор темной темы

--- app/src/main/java/com/utmaximur/alcoholtracker/App.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/App.kt
@@ -1,9 +1,13 @@
 package com.utmaximur.alcoholtracker
 
 import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatDelegate
 import com.utmaximur.alcoholtracker.dagger.component.AlcoholTrackComponent
 import com.utmaximur.alcoholtracker.dagger.component.DaggerAlcoholTrackComponent
 import com.utmaximur.alcoholtracker.dagger.module.RoomDatabaseModule
+import com.utmaximur.alcoholtracker.ui.settings.view.*
 
 open class App: Application() {
 
@@ -12,8 +16,15 @@ open class App: Application() {
     }
     lateinit var alcoholTrackComponent: AlcoholTrackComponent
 
+    private val sharedPrefs by lazy {  getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE) }
+
     override fun onCreate() {
         super.onCreate()
+        when(getSavedTheme()){
+            THEME_DARK -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            THEME_LIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            THEME_UNDEFINED -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
+        }
         instance = this
 
         alcoholTrackComponent = DaggerAlcoholTrackComponent
@@ -21,4 +32,6 @@ open class App: Application() {
             .roomDatabaseModule(RoomDatabaseModule(this))
             .build()
     }
+
+    private fun getSavedTheme() = sharedPrefs?.getInt(KEY_THEME, THEME_UNDEFINED)
 }
--- app/src/main/java/com/utmaximur/alcoholtracker/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/ui/settings/view/SettingsFragment.kt
@@ -2,18 +2,28 @@ package com.utmaximur.alcoholtracker.ui.settings.view
 
 
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.utmaximur.alcoholtracker.BuildConfig
 import com.utmaximur.alcoholtracker.R
 
+const val PREFS_NAME = "theme_prefs"
+const val KEY_THEME = "prefs.theme"
+const val THEME_UNDEFINED = -1
+const val THEME_LIGHT = 0
+const val THEME_DARK = 1
 
 class SettingsFragment : Fragment() {
 
@@ -21,6 +31,9 @@ class SettingsFragment : Fragment() {
     private lateinit var termsOfUseLayout: Button
     private lateinit var rateUsButton: Button
     private lateinit var versionApp: TextView
+    private lateinit var themeSwitch: SwitchMaterial
+
+    private val sharedPrefs by lazy {  activity?.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,11 +50,13 @@ class SettingsFragment : Fragment() {
         termsOfUseLayout = view.findViewById(R.id.terms_of_use_button)
         rateUsButton = view.findViewById(R.id.rate_app_button)
         versionApp = view.findViewById(R.id.version_app)
+        themeSwitch = view.findViewById(R.id.theme_switch)
     }
 
 
     private fun initUi(view: View) {
         findViewById(view)
+        initTheme()
 
         privacyPolicyLayout.setOnClickListener {
 
@@ -55,8 +70,38 @@ class SettingsFragment : Fragment() {
             rateUs()
         }
 
+        themeSwitch.setOnCheckedChangeListener { _, b ->
+            if(b){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                saveTheme(THEME_DARK)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                saveTheme(THEME_LIGHT)
+            }
+        }
+
         versionApp.text = BuildConfig.VERSION_NAME
     }
+
+    private fun initTheme() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            themeSwitch.visibility = View.VISIBLE
+        } else {
+            themeSwitch.visibility = View.GONE
+        }
+        when (getSavedTheme()) {
+            THEME_DARK -> themeSwitch.isChecked = true
+            THEME_UNDEFINED -> {
+                when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_YES -> themeSwitch.isChecked = true
+                }
+            }
+        }
+    }
+
+    private fun saveTheme(theme: Int) = sharedPrefs?.edit()?.putInt(KEY_THEME, theme)?.apply()
+
+    private fun getSavedTheme() = sharedPrefs?.getInt(KEY_THEME, THEME_UNDEFINED)
 
     private fun rateUs() {
         val appPackageName: String = requireContext().packageName
@@ -75,9 +120,5 @@ class SettingsFragment : Fragment() {
                 )
             )
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
     }
 }
