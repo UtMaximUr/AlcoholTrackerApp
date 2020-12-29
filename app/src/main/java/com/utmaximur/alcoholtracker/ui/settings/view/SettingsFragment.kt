469d78818a2cf2e6247a4c0423a411365653791e
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2020-12-29 21:27:31 +0700

    Добавлен выбор темы

--- app/src/main/java/com/utmaximur/alcoholtracker/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/ui/settings/view/SettingsFragment.kt
@@ -8,6 +8,7 @@ import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,9 +16,11 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.switchmaterial.SwitchMaterial
 import com.utmaximur.alcoholtracker.BuildConfig
 import com.utmaximur.alcoholtracker.R
+import com.utmaximur.alcoholtracker.ui.settings.view.adapter.ThemeListAdapter
 
 const val PREFS_NAME = "theme_prefs"
 const val KEY_THEME = "prefs.theme"
@@ -25,15 +28,21 @@ const val THEME_UNDEFINED = -1
 const val THEME_LIGHT = 0
 const val THEME_DARK = 1
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), ThemeListAdapter.ThemeListener {
 
     private lateinit var privacyPolicyLayout: Button
     private lateinit var termsOfUseLayout: Button
     private lateinit var rateUsButton: Button
     private lateinit var versionApp: TextView
     private lateinit var themeSwitch: SwitchMaterial
+    private lateinit var themeList: RecyclerView
 
-    private val sharedPrefs by lazy {  activity?.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE) }
+    private val sharedPrefs by lazy {
+        activity?.getSharedPreferences(
+            PREFS_NAME,
+            Context.MODE_PRIVATE
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,6 +60,7 @@ class SettingsFragment : Fragment() {
         rateUsButton = view.findViewById(R.id.rate_app_button)
         versionApp = view.findViewById(R.id.version_app)
         themeSwitch = view.findViewById(R.id.theme_switch)
+        themeList = view.findViewById(R.id.theme_list)
     }
 
 
@@ -70,13 +80,20 @@ class SettingsFragment : Fragment() {
             rateUs()
         }
 
+        themeList.adapter = ThemeListAdapter(
+            requireContext().resources.getStringArray(R.array.theme_array).toList(),
+            getTheme()
+        )
+        (themeList.adapter as ThemeListAdapter).setListener(this)
+
         themeSwitch.setOnCheckedChangeListener { _, b ->
-            if(b){
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                saveTheme(THEME_DARK)
-            }else{
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                saveTheme(THEME_LIGHT)
+            if (b) {
+                Log.e("fix", " getDefaultNightMode() ${AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM}")
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
+                themeList.visibility = View.GONE
+                saveTheme(THEME_UNDEFINED)
+            } else {
+                themeList.visibility = View.VISIBLE
             }
         }
 
@@ -84,24 +101,52 @@ class SettingsFragment : Fragment() {
     }
 
     private fun initTheme() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             themeSwitch.visibility = View.VISIBLE
         } else {
             themeSwitch.visibility = View.GONE
         }
         when (getSavedTheme()) {
-            THEME_DARK -> themeSwitch.isChecked = true
+            THEME_DARK -> {
+                themeSwitch.isChecked = false
+                themeList.visibility = View.VISIBLE
+            }
+            THEME_LIGHT -> {
+                themeSwitch.isChecked = false
+                themeList.visibility = View.VISIBLE
+            }
             THEME_UNDEFINED -> {
-                when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
-                    Configuration.UI_MODE_NIGHT_YES -> themeSwitch.isChecked = true
-                }
+                themeSwitch.isChecked = true
             }
         }
     }
 
-    private fun saveTheme(theme: Int) = sharedPrefs?.edit()?.putInt(KEY_THEME, theme)?.apply()
+    override fun saveTheme(theme: Int) {
+        sharedPrefs?.edit()?.putInt(KEY_THEME, theme)?.apply()
+        when (theme) {
+            THEME_DARK -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            THEME_LIGHT -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            THEME_UNDEFINED -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
+            }
+        }
+    }
 
     private fun getSavedTheme() = sharedPrefs?.getInt(KEY_THEME, THEME_UNDEFINED)
+
+    private fun getTheme(): Int {
+        return if (resources.configuration.uiMode and
+            Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+        ) {
+            THEME_DARK
+        } else {
+            THEME_LIGHT
+        }
+    }
 
     private fun rateUs() {
         val appPackageName: String = requireContext().packageName
---
+++ app/src/main/java/com/utmaximur/alcoholtracker/ui/settings/view/adapter/ThemeListAdapter.kt
@@ -0,0 +1,59 @@
+package com.utmaximur.alcoholtracker.ui.settings.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.utmaximur.alcoholtracker.R
+
+class ThemeListAdapter(private val themeList: List<String>, private val themeId: Int)  :
+    RecyclerView.Adapter<ThemeListAdapter.ViewHolder>() {
+
+    interface ThemeListener {
+        fun saveTheme(theme: Int)
+    }
+
+    private lateinit var listener: ThemeListener
+
+    fun setListener(listener: ThemeListener){
+        this.listener = listener
+    }
+
+    class ViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.item_theme, parent, false)) {
+        private var themeText: TextView? = null
+        private var themeChoice: ImageView? = null
+
+        init {
+            themeText = itemView.findViewById(R.id.item_theme_text)
+            themeChoice = itemView.findViewById(R.id.item_theme_choice)
+        }
+
+        fun bind(theme: String, themeId: Int, position: Int, listener: ThemeListener) {
+            themeText?.text = theme
+            themeText?.setOnClickListener {
+                listener.saveTheme(position)
+            }
+            if(themeId == position){
+                themeChoice?.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+        return ViewHolder(view, parent)
+    }
+
+    override fun getItemCount(): Int {
+        return themeList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val theme: String = themeList[position]
+        holder.bind(theme, themeId, position, listener)
+    }
+}
