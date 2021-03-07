308401dd4226ae416dc012d94a4f55c2975618ce
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2021-03-07 11:27:05 +0700

    Добавлен фрагмент подтверждения выбора даты

---
+++ app/src/main/java/com/utmaximur/alcoholtracker/ui/dialog/AddDrinkDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.utmaximur.alcoholtracker.ui.dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.utmaximur.alcoholtracker.R
+
+
+class AddDrinkDialogFragment : DialogFragment() {
+
+    private var addDrinkDialogListener: AddDrinkDialogListener? = null
+
+    interface AddDrinkDialogListener {
+        fun onAddDrinkDialog()
+    }
+
+    fun setListener(addDrinkDialogListener: AddDrinkDialogListener) {
+        this.addDrinkDialogListener = addDrinkDialogListener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(getText(R.string.dialog_select_date_title))
+                .setPositiveButton(getText(R.string.dialog_select_date_ok)) { dialog, _ ->
+                    addDrinkDialogListener?.onAddDrinkDialog()
+                    dialog.cancel()
+                }
+                .setNegativeButton(getText(R.string.dialog_select_date_no)) {
+                        dialog, _ ->
+                    addDrinkDialogListener?.onAddDrinkDialog()
+                    dialog.cancel()
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
