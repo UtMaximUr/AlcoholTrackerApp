0400351d94d49af8a1737b3ccb032e3f6bbbc793
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2021-03-14 18:52:11 +0700

    refactoring code

--- app/src/main/java/com/utmaximur/alcoholtracker/data/AlcoholTrackDatabase.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/data/AlcoholTrackDatabase.kt
@@ -8,13 +8,10 @@ import com.utmaximur.alcoholtracker.data.model.AlcoholTrack
 import com.utmaximur.alcoholtracker.data.model.Drink
 import com.utmaximur.alcoholtracker.data.dao.AlcoholTrackDao
 import com.utmaximur.alcoholtracker.data.dao.DrinkDao
-import com.utmaximur.alcoholtracker.data.dao.IconDao
-import com.utmaximur.alcoholtracker.data.model.Icon
 
-@Database(entities = [Drink::class, AlcoholTrack::class, Icon::class], version = 2)
+@Database(entities = [Drink::class, AlcoholTrack::class], version = 2)
 @TypeConverters(Converters::class)
 abstract class AlcoholTrackDatabase: RoomDatabase(){
     abstract fun getDrinkDao(): DrinkDao
     abstract fun getTrackDao(): AlcoholTrackDao
-    abstract fun getIconDao(): IconDao
 }
