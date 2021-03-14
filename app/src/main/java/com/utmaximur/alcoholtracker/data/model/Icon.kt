121b0a4be598e2bffea74812a6f5ee35659d25c6
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2021-03-14 18:51:47 +0700

    model change

--- app/src/main/java/com/utmaximur/alcoholtracker/data/model/Icon.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/data/model/Icon.kt
@@ -1,12 +1,6 @@
 package com.utmaximur.alcoholtracker.data.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "icon_database")
 data class Icon(
-    @PrimaryKey
-    var id: String,
     var icon: Int
-
 )
