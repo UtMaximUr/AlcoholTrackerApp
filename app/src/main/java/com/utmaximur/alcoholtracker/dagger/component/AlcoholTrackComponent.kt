028bb4f0d2ddd6158fd9ea01e7bd33a161f7ce8f
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2021-03-14 18:48:35 +0700

    add inject AddNewDrink.kt

--- app/src/main/java/com/utmaximur/alcoholtracker/dagger/component/AlcoholTrackComponent.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/dagger/component/AlcoholTrackComponent.kt
@@ -6,6 +6,7 @@ import com.utmaximur.alcoholtracker.repository.DrinkRepository
 import com.utmaximur.alcoholtracker.repository.IconRepository
 import com.utmaximur.alcoholtracker.repository.TrackRepository
 import com.utmaximur.alcoholtracker.ui.add.presentation.view.AddFragment
+import com.utmaximur.alcoholtracker.ui.addmydrink.view.AddNewDrink
 import com.utmaximur.alcoholtracker.ui.calculator.view.CalculatorFragment
 import com.utmaximur.alcoholtracker.ui.calendar.presentation.view.CalendarFragment
 import com.utmaximur.alcoholtracker.ui.dialog.addicon.AddIconDrinkDialogFragment
@@ -28,6 +29,8 @@ interface AlcoholTrackComponent {
     fun inject(addFragment: AddFragment)
 
     fun inject(addIconDrinkDialogFragment: AddIconDrinkDialogFragment)
+
+    fun inject(addNewDrink: AddNewDrink)
 
     fun inject(calculatorFragment: CalculatorFragment)
 
