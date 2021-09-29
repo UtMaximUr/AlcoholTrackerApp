eb2b31b9521f86d3d5854fbd4764b16dc3274baf
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2021-09-29 20:41:46 +0700

    rename package

--- app/src/main/java/com/utmaximur/alcoholtracker/data/AlcoholTrackDatabase.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/data/AlcoholTrackDatabase.kt
@@ -4,12 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.utmaximur.alcoholtracker.data.conversion.Converters
-import com.utmaximur.alcoholtracker.data.model.AlcoholTrack
-import com.utmaximur.alcoholtracker.data.model.Drink
+import com.utmaximur.alcoholtracker.data.dbo.TrackDBO
+import com.utmaximur.alcoholtracker.data.dbo.DrinkDBO
 import com.utmaximur.alcoholtracker.data.dao.AlcoholTrackDao
 import com.utmaximur.alcoholtracker.data.dao.DrinkDao
 
-@Database(entities = [Drink::class, AlcoholTrack::class], version = 4)
+@Database(entities = [DrinkDBO::class, TrackDBO::class], version = 4)
 @TypeConverters(Converters::class)
 abstract class AlcoholTrackDatabase: RoomDatabase(){
     abstract fun getDrinkDao(): DrinkDao
