872ce04a9489e7a21089749864c5515182eb4612
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2021-09-30 21:08:26 +0700

    replace livedata on coroutines

--- app/src/main/java/com/utmaximur/alcoholtracker/data/dao/AlcoholTrackDao.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/data/dao/AlcoholTrackDao.kt
@@ -1,24 +1,24 @@
 package com.utmaximur.alcoholtracker.data.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.utmaximur.alcoholtracker.data.model.AlcoholTrack
+import com.utmaximur.alcoholtracker.data.dbo.TrackDBO
 
 @Dao
 interface AlcoholTrackDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertTrack(track: AlcoholTrack)
+    suspend fun insertTrack(trackDBO: TrackDBO)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun updateTrack(track: AlcoholTrack)
+    suspend fun updateTrack(trackDBO: TrackDBO)
 
     @Delete
-    fun deleteTrack(track: AlcoholTrack)
-
-    @Query("SELECT * FROM track_database")
-    fun getTracks(): LiveData<MutableList<AlcoholTrack>>
+    suspend fun deleteTrack(trackDBO: TrackDBO)
 
     @Query("SELECT * FROM track_database WHERE date == :date")
-    fun getTrack(date: Long): LiveData<AlcoholTrack?>
+    suspend fun getTrack(date: Long): TrackDBO
+
+    @Query("SELECT * FROM track_database")
+    suspend fun getTracks(): List<TrackDBO>
+
 }
