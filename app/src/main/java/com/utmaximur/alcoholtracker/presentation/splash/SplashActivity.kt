af12a393427245bc42e1669edd30853913d282a3
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-01-23 00:21:36 +0700

    replace xml on jetpack compose

--- app/src/main/java/com/utmaximur/alcoholtracker/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/splash/SplashActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
@@ -54,15 +53,6 @@ class SplashActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@ExperimentalAnimationApi
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AlcoholTrackerTheme {
-        SplashView()
     }
 }
 
