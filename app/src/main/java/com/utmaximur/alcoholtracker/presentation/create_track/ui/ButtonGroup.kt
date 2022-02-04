70071dd7d816448c4b021623bb86f17756ef797c
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-04 17:58:02 +0700

    replace xml on jetpack compose

---
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/create_track/ui/ButtonGroup.kt
@@ -0,0 +1,98 @@
+package com.utmaximur.alcoholtracker.presentation.create_track.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.utmaximur.alcoholtracker.R
+
+@ExperimentalAnimationApi
+@Composable
+fun ButtonGroup(
+    dateState: String,
+    visibleTodayState: Boolean,
+    onSelectDateClick: () -> Unit,
+    onTodayClick: () -> Unit
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 12.dp)
+    ) {
+        Box(modifier = Modifier.weight(1f)) {
+            Button(
+                text = dateState,
+                onSelectDateClick = onSelectDateClick
+            )
+        }
+        AnimatedVisibility(
+            visible = visibleTodayState,
+            enter = expandVertically()
+        ) {
+            OutlineButton(
+                text = R.string.add_today,
+                onTodayClick = onTodayClick
+            )
+        }
+    }
+}
+
+@Composable
+fun Button(
+    text: String,
+    size: TextUnit = 16.sp,
+    radius: Int = 50,
+    onSelectDateClick: () -> Unit
+) {
+    androidx.compose.material.Button(
+        modifier = Modifier
+            .padding(end = 2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(radius),
+        onClick = { onSelectDateClick() }
+    ) {
+        Text(
+            text = text,
+            fontSize = size,
+            fontFamily = FontFamily(Font(R.font.roboto_condensed_regular)),
+            color = colorResource(id = R.color.text_color_white)
+        )
+    }
+}
+
+@Composable
+fun OutlineButton(
+    text: Int,
+    size: TextUnit = 16.sp,
+    radius: Int = 50,
+    onTodayClick: () -> Unit) {
+    OutlinedButton(
+        modifier = Modifier
+            .padding(start = 2.dp),
+        shape = RoundedCornerShape(radius),
+        onClick = { onTodayClick() }
+    ) {
+        Text(
+            text = stringResource(id = text),
+            fontSize = size,
+            fontFamily = FontFamily(Font(R.font.roboto_condensed_regular)),
+            color = colorResource(id = R.color.text_color)
+        )
+    }
+}
