a2d2587544fbd6467a75465ec3af344f9b8dc9f2
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-07 19:23:59 +0700

    refactoring

--- app/src/main/java/com/utmaximur/alcoholtracker/util/ViewUtils.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/util/ViewUtils.kt
@@ -14,12 +14,8 @@ import android.view.animation.AnimationUtils
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
-import androidx.core.view.children
-import androidx.viewpager.widget.ViewPager
 import com.google.android.material.snackbar.Snackbar
 import com.utmaximur.alcoholtracker.R
-import com.utmaximur.alcoholtracker.util.extension.empty
-import java.util.*
 
 fun View.hideKeyboard() {
     (this.context.getSystemService(Service.INPUT_METHOD_SERVICE) as? InputMethodManager)
@@ -41,11 +37,6 @@ fun View.toInvisible() {
 
 fun View.snackBar(message: String) {
     Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
-}
-
-fun View.alphaView() {
-    val animation: Animation = AnimationUtils.loadAnimation(this.context, R.anim.alpha_1000)
-    this.startAnimation(animation)
 }
 
 fun View.alphaViewIn() {
@@ -75,48 +66,6 @@ fun Context.getDisplayWidth(): Int {
     }
 }
 
-private fun NumberPicker.initNumberPicker() {
-    this.children.iterator().forEach {
-        if (it is EditText) it.width = LinearLayout.LayoutParams.WRAP_CONTENT
-    }
-}
-
-fun NumberPicker.settingsNumberPicker() {
-    this.maxValue = 10
-    this.minValue = 1
-}
-
-fun NumberPicker.settingsNumberPicker(maxValue: Int, displayedValues: Array<String?>) {
-    this.displayedValues = null
-    this.maxValue = maxValue - 1
-    this.displayedValues = displayedValues
-    this.initNumberPicker()
-}
-
-fun NumberPicker.resetSettingsNumberPicker(maxValue: Int, displayedValues: Array<String?>) {
-    this.displayedValues = null
-    this.minValue = 0
-    this.maxValue = maxValue
-    this.displayedValues = displayedValues
-}
-
-fun ViewPager.addOnPageChangeListener(func: (Int) -> Unit, hide: () -> Unit) {
-    this.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-        override fun onPageScrollStateChanged(state: Int) {}
-        override fun onPageScrolled(
-            position: Int,
-            positionOffset: Float,
-            positionOffsetPixels: Int
-        ) {
-            hide()
-        }
-
-        override fun onPageSelected(position: Int) {
-            func(position)
-        }
-    })
-}
-
 fun EditText.onEditorActionListener(hide: () -> Unit) {
     this.setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
         if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -131,12 +80,4 @@ fun View.setupFullHeight() {
     val layoutParams = this.layoutParams
     layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
     this.layoutParams = layoutParams
-}
-
-fun View.setTextOrEmpty(view: TextView, text: Any) {
-    if (text == String.empty()) {
-        this.toGone()
-    } else {
-        view.text = text.toString()
-    }
 }
