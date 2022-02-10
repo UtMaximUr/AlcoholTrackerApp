7c0802197fb2d9286a934cfe6b31774174dfa82a
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-10 18:50:58 +0700

    replace on jetpack compose

--- app/src/main/java/com/utmaximur/alcoholtracker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/main/MainActivity.kt
@@ -3,69 +3,27 @@ package com.utmaximur.alcoholtracker.presentation.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.utmaximur.alcoholtracker.R
 import com.utmaximur.alcoholtracker.data.update.UpdateManager
 import com.utmaximur.alcoholtracker.data.update.UpdateManager.Companion.getInstance
-import com.utmaximur.alcoholtracker.databinding.ActivityMainBinding
-import com.utmaximur.alcoholtracker.presentation.create_track.CreateTrackFragment
-import com.utmaximur.alcoholtracker.presentation.create_my_drink.CreateMyDrink
+import com.utmaximur.alcoholtracker.presentation.main.ui.MainScreen
 import com.utmaximur.alcoholtracker.util.*
+import dagger.hilt.android.AndroidEntryPoint
 
-
-class MainActivity : AppCompatActivity(),
-    CreateTrackFragment.AddFragmentListener,
-    CreateMyDrink.AddNewFragmentListener,
-    UpdateManager.UpdateListener {
-
-    private lateinit var navHostFragment: NavHostFragment
-    private lateinit var navController: NavController
-
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), UpdateManager.UpdateListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initView()
-    }
-
-    private fun initView() = with(binding) {
-        this@MainActivity.navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = this@MainActivity.navHostFragment.navController
-        bottomNavigationView.setupWithNavController(navController)
-        bottomNavigationView.background = null
-        addTrack.setOnClickListener { navController.navigate(R.id.addFragment) }
-    }
-
-    override fun onShowAddNewDrinkFragment() {
-        navController.navigate(R.id.addNewDrinkFragment)
-    }
-
-    override fun onShowEditNewDrinkFragment(bundle: Bundle) {
-        navController.navigate(R.id.addNewDrinkFragment, bundle)
+        setContent {
+            MainScreen()
+        }
     }
 
     override fun onShowUpdateDialog() {
-        navController.navigate(R.id.updateBottomDialogFragment)
-    }
-
-    override fun closeFragment() {
-        navController.popBackStack()
-    }
-
-    override fun onHideNavigationBar() = with(binding) {
-        coordinatorLayout.toGone()
-    }
-
-    override fun onShowNavigationBar() = with(binding) {
-        coordinatorLayout.toVisible()
+//        navController.navigate(R.id.updateBottomDialogFragment)
     }
 
     override fun onStart() {
