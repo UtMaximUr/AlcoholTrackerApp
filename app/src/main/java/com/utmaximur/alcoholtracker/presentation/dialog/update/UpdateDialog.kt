088363819ef8d3c61eb3649d8874115ba519e01a
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-13 22:53:20 +0700

    replace xml on compose

---
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/dialog/update/UpdateDialog.kt
@@ -0,0 +1,53 @@
+package com.utmaximur.alcoholtracker.presentation.dialog.update
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.utmaximur.alcoholtracker.R
+
+@Composable
+fun UpdateDialog(
+    onLaterClick: () -> Unit,
+    onNowClick: () -> Unit
+) {
+
+    Column {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_update_black_24dp),
+                contentDescription = null,
+                tint = colorResource(id = R.color.text_color)
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                text = stringResource(id = R.string.update_title),
+                color = colorResource(id = R.color.text_color)
+            )
+        }
+        Row(modifier = Modifier.padding(16.dp)) {
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { onNowClick() })
+            {
+                Text(text = stringResource(id = R.string.update_restart))
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { onLaterClick() })
+            {
+                Text(text = stringResource(id = R.string.update_later))
+            }
+        }
+    }
+}
