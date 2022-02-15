2e7efdfc35a240fda89b689255649e382038e4e7
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-15 18:19:45 +0700

    delete unused file

--- app/src/main/java/com/utmaximur/alcoholtracker/presentation/splash/ui/theme/Color.kt
+++
@@ -1,6 +0,0 @@
-package com.utmaximur.alcoholtracker.presentation.splash.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val AccentColorDark = Color(0xFFBB86FC)
-val AccentColorLight = Color(0xFFD50000)
