5795fbdbbcb4fc44f7d138cc15742ed16b189c33
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-16 22:17:29 +0700

    add colors compose

--- app/src/main/java/com/utmaximur/alcoholtracker/presentation/main/ui/theme/Theme.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/main/ui/theme/Theme.kt
@@ -1,17 +1,24 @@
-package com.utmaximur.alcoholtracker.presentation.splash.ui.theme
+package com.utmaximur.alcoholtracker.presentation.main.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = AccentColorDark
+    primary = AccentColorDark,
+    background = BackgroundDark,
+    surface = Color(0xFF121212),
+    onPrimary = TextColorDark
 )
 
 private val LightColorPalette = lightColors(
-    primary = AccentColorLight
+    primary = AccentColorLight,
+    background = BackgroundLight,
+    surface = Color.White,
+    onPrimary = TextColorLight
 )
 
 @Composable
