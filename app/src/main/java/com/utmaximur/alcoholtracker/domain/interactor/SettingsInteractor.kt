265b8fafb2e64078b1d069ce58621f315dd1fb4f
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-17 10:38:40 +0700

    add install theme by system choice

--- app/src/main/java/com/utmaximur/alcoholtracker/domain/interactor/SettingsInteractor.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/domain/interactor/SettingsInteractor.kt
@@ -1,40 +1,12 @@
 package com.utmaximur.alcoholtracker.domain.interactor
 
 import com.utmaximur.alcoholtracker.data.repository.PreferencesRepository
-import com.utmaximur.alcoholtracker.util.THEME_DARK
-import com.utmaximur.alcoholtracker.util.THEME_LIGHT
-import com.utmaximur.alcoholtracker.util.THEME_UNDEFINED
 
 import javax.inject.Inject
 
 class SettingsInteractor @Inject constructor(
     private val preferencesRepository: PreferencesRepository
 ) {
-
-    private var isUseDefaultTheme: Boolean = false
-    private var isUseLightTheme: Boolean = false
-    private var isUseDarkTheme: Boolean = false
-
-    init {
-        when (preferencesRepository.getSelectedTheme()) {
-            THEME_DARK -> {
-                isUseDarkTheme = !isUseDarkTheme
-            }
-            THEME_LIGHT -> {
-                isUseLightTheme = !isUseLightTheme
-            }
-            THEME_UNDEFINED -> {
-                isUseDefaultTheme = !isUseDefaultTheme
-//                if (resources.configuration.uiMode and
-//                    Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
-//                ) {
-//                    isUseDarkTheme = !isUseDarkTheme
-//                } else {
-//                    isUseLightTheme = !isUseLightTheme
-//                }
-            }
-        }
-    }
 
     fun saveUpdateChecked(checked: Boolean) =
         preferencesRepository.saveUpdateChecked(checked)
@@ -43,10 +15,4 @@ class SettingsInteractor @Inject constructor(
         preferencesRepository.saveThemeChecked(theme)
 
     fun isUpdateChecked() = preferencesRepository.isUpdateChecked()
-
-    fun isUseDefaultThemeChecked() = isUseDefaultTheme
-
-    fun isUseLightThemeChecked() = isUseLightTheme
-
-    fun isUseDarkThemeChecked() = isUseDarkTheme
 }
--- app/src/main/java/com/utmaximur/alcoholtracker/presentation/settings/SettingsViewModel.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/settings/SettingsViewModel.kt
@@ -16,40 +16,26 @@ import javax.inject.Inject
 class SettingsViewModel @Inject constructor(private val settingsInteractor: SettingsInteractor) :
     ViewModel() {
 
-    val checkedThemeState: LiveData<Boolean> by lazy { MutableLiveData() }
     val checkedUpdateState: LiveData<Boolean> by lazy { MutableLiveData() }
-    val themeLightCheckState: LiveData<Boolean> by lazy { MutableLiveData() }
-    val themeDarkCheckState: LiveData<Boolean> by lazy { MutableLiveData() }
 
     init {
-        checkedThemeState.setValue(settingsInteractor.isUseDefaultThemeChecked())
         checkedUpdateState.setValue(settingsInteractor.isUpdateChecked())
-        themeLightCheckState.setValue(settingsInteractor.isUseLightThemeChecked())
-        themeDarkCheckState.setValue(settingsInteractor.isUseDarkThemeChecked())
     }
 
     fun onLightThemeChange(checked: Boolean) {
         if (checked) {
-            themeLightCheckState.setValue(checked)
             settingsInteractor.saveThemeChecked(THEME_LIGHT)
-        }
-        if (themeDarkCheckState.value!!) {
-            themeDarkCheckState.setValue(!checked)
         }
     }
 
     fun onDarkThemeChange(checked: Boolean) {
         if (checked) {
-            themeDarkCheckState.setValue(checked)
             settingsInteractor.saveThemeChecked(THEME_DARK)
-        }
-        if (themeLightCheckState.value!!) {
-            themeLightCheckState.setValue(!checked)
         }
     }
 
     fun onUseDefaultThemeChange(checked: Boolean) {
-        checkedThemeState.setValue(checked)
+        if (checked)
         settingsInteractor.saveThemeChecked(THEME_UNDEFINED)
     }
 
