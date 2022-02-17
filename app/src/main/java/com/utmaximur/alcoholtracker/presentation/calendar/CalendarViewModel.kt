c1a3a342030f83f6881b0b32ea8b1462fc2879de
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-02-17 13:13:50 +0700

    delete unused fun

--- app/src/main/java/com/utmaximur/alcoholtracker/presentation/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/utmaximur/alcoholtracker/presentation/calendar/CalendarViewModel.kt
@@ -6,27 +6,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.utmaximur.alcoholtracker.domain.entity.Track
 import com.utmaximur.alcoholtracker.domain.interactor.CalendarInteractor
-import com.utmaximur.alcoholtracker.util.setValue
+import com.utmaximur.alcoholtracker.presentation.calendar.state.TextState
+import com.utmaximur.alcoholtracker.util.setPostValue
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
+@HiltViewModel
 open class CalendarViewModel @Inject constructor(private var calendarInteractor: CalendarInteractor) :
     ViewModel() {
-
-    val tracksByDay: LiveData<List<Track>> by lazy {
-        MutableLiveData()
-    }
 
     val tracks: LiveData<List<Track>> by lazy {
         MutableLiveData()
     }
 
-    init {
-        viewModelScope.launch {
-            val dataTracks = dataTracks()
-            (tracks as MutableLiveData).value = dataTracks
-        }
+    val textState: LiveData<TextState> by lazy {
+        MutableLiveData()
     }
 
     private suspend fun dataTracks(): List<Track> {
@@ -40,28 +36,31 @@ open class CalendarViewModel @Inject constructor(private var calendarInteractor:
         }
     }
 
-    suspend fun dataAlcoholTrackByDay(
-        eventDay: Long
-    ): List<Track> {
+    private suspend fun dataAlcoholTrackByDay(eventDay: Long): List<Track> {
         return calendarInteractor.getAlcoholTrackByDay(eventDay)
     }
 
-    fun initTracksByDay(date: Long) = viewModelScope.launch {
+    fun initTextState(date: Long) = viewModelScope.launch {
+        val dataTracks = dataTracks()
+        if (dataTracks.isEmpty()) {
+            textState.setPostValue(TextState.AddPressToStart())
+        } else {
+            val dataAlcoholTrackByDay = dataAlcoholTrackByDay(date)
+            if (dataAlcoholTrackByDay.isEmpty()) {
+                textState.setPostValue(TextState.TracksEmpty())
+            } else {
+                textState.setPostValue(TextState.SelectDay())
+            }
+        }
+    }
+
+    suspend fun isTrackByDayEmpty(date: Long): Boolean {
         val dataTracks = dataAlcoholTrackByDay(date)
-        tracksByDay.setValue(dataTracks)
-    }
-
-    private fun updateTracksByDay(eventDay: Long) = viewModelScope.launch {
-        val dataTracks = dataAlcoholTrackByDay(eventDay)
-        tracksByDay.setValue(dataTracks)
-    }
-
-    suspend fun dataTrack(date: Long): Track {
-        return calendarInteractor.getTrack(date)
-    }
-
-    suspend fun onDeleteDrink(trackCalendar: Track) {
-        calendarInteractor.deleteTrack(trackCalendar)
-        updateTracksByDay(trackCalendar.date)
+        if (dataTracks.isEmpty()) {
+            textState.setPostValue(TextState.TracksEmpty())
+        } else {
+            textState.setPostValue(TextState.SelectDay())
+        }
+        return dataTracks.isEmpty()
     }
 }
