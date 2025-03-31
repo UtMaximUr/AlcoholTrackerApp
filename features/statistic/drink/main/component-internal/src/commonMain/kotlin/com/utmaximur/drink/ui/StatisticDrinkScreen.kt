e10e77f514cfe3acc108829361cd69a26835a3bf
maksimutuznikov <biwjuk-zickyk-4Sycga>
2025-03-31 11:04:35 +0700

    remove unused import

--- features/statistic/drink/main/component-internal/src/commonMain/kotlin/com/utmaximur/drink/ui/StatisticDrinkScreen.kt
+++ features/statistic/drink/main/component-internal/src/commonMain/kotlin/com/utmaximur/drink/ui/StatisticDrinkScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
