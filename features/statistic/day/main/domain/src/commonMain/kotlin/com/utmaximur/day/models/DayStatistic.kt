89e98e4e1ffd602bd88cc485b8265618827aad55
maksimutuznikov <biwjuk-zickyk-4Sycga>
2025-04-06 17:29:43 +0700

    refactoring

--- features/statistic/day/main/domain/src/commonMain/kotlin/com/utmaximur/day/models/DayStatistic.kt
+++ features/statistic/day/main/domain/src/commonMain/kotlin/com/utmaximur/day/models/DayStatistic.kt
@@ -1,7 +1,9 @@
 package com.utmaximur.day.models
 
+import com.utmaximur.domain.ZERO_VALUE
+
 data class DayStatistic(
     val statisticDay: StatisticDay,
-    val countsDaysInYear: Int = 0,
+    val countsDaysInYear: Int = ZERO_VALUE,
     val countsDays: Int
 )
