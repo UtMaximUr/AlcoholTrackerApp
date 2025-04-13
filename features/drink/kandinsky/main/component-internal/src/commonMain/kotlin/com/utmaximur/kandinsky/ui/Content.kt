4fc7ac64a65dd3896e3a8d536394beb737708e53
maksimutuznikov <biwjuk-zickyk-4Sycga>
2025-04-13 18:54:03 +0700

    refactoring design

--- features/drink/kandinsky/main/component-internal/src/commonMain/kotlin/com/utmaximur/kandinsky/ui/Content.kt
+++ features/drink/kandinsky/main/component-internal/src/commonMain/kotlin/com/utmaximur/kandinsky/ui/Content.kt
@@ -1,8 +1,5 @@
 package com.utmaximur.kandinsky.ui
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,11 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.utmaximur.core.mvi_mapper.RequestUi
 import com.utmaximur.design.RequestWidget
@@ -27,7 +21,6 @@ import com.utmaximur.domain.kandinsky.ImageStyle
 import com.utmaximur.kandinsky.GenerateImageData
 import features.drink.kandinsky.main.Res
 import features.drink.kandinsky.main.create_generation
-import features.drink.kandinsky.main.generation_progress
 import features.drink.kandinsky.main.title_enter_text
 import org.jetbrains.compose.resources.stringResource
 
@@ -49,24 +42,6 @@ internal fun Content(
             .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-        AnimatedVisibility(
-            visible = isProgressStatus,
-            enter = expandVertically(),
-            exit = shrinkVertically()
-        ) {
-            ElevatedCardApp(
-                shape = MaterialTheme.shapes.large
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    text = stringResource(Res.string.generation_progress),
-                    style = MaterialTheme.typography.titleSmall,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
         GenerateImageContent(
             generationResult = generationResult
         )
--- features/drink/kandinsky/main/component-internal/src/commonMain/kotlin/com/utmaximur/kandinsky/ui/GenerateImageContent.kt
+++ features/drink/kandinsky/main/component-internal/src/commonMain/kotlin/com/utmaximur/kandinsky/ui/GenerateImageContent.kt
@@ -1,15 +1,25 @@
 package com.utmaximur.kandinsky.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil3.compose.SubcomposeAsyncImage
 import com.utmaximur.design.extensions.shimmer
@@ -19,6 +29,7 @@ import com.utmaximur.domain.kandinsky.GenerationStatus
 import features.drink.kandinsky.main.Res
 import features.drink.kandinsky.main.cd_generate_image
 import features.drink.kandinsky.main.error_placeholder_image
+import features.drink.kandinsky.main.generation_progress
 import features.drink.kandinsky.main.placeholder_image
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
@@ -41,9 +52,11 @@ internal fun GenerateImageContent(
                 contentDescription = stringResource(Res.string.cd_generate_image),
                 error = { ImageHolder(Res.drawable.placeholder_image) }
             )
+
             GenerationStatus.FAIL -> ImageHolder(
                 resource = Res.drawable.error_placeholder_image
             )
+
             else -> ImageHolder(
                 resource = Res.drawable.placeholder_image,
                 loading = generationResult.isStatusProgress
@@ -56,13 +69,31 @@ internal fun GenerateImageContent(
 private fun ImageHolder(
     resource: DrawableResource,
     loading: Boolean = false
-) = Image(
-    modifier = Modifier
-        .shimmer(loading)
-        .fillMaxSize(),
-    painter = painterResource(resource),
-    contentDescription = stringResource(Res.string.cd_generate_image)
-)
+) = Box(
+    contentAlignment = Alignment.Center
+) {
+    Image(
+        modifier = Modifier
+            .shimmer(loading)
+            .fillMaxSize(),
+        painter = painterResource(resource),
+        contentDescription = stringResource(Res.string.cd_generate_image)
+    )
+    AnimatedVisibility(
+        visible = loading,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            text = stringResource(Res.string.generation_progress),
+            style = MaterialTheme.typography.titleSmall,
+            textAlign = TextAlign.Center
+        )
+    }
+}
 
 private fun Modifier.applyBlur(apply: Boolean): Modifier = composed {
     return@composed if (apply) this.blur(
